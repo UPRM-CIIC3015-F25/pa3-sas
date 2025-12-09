95403321a948d6a9f57fe6c95108d6c92126e4d8
JanPaulAyala-UPRM <jan.ayala2@upr.edu>
2025-12-08 21:41:38 -0400

    Completed Task 6.1, added Planets dictionary

--- Cards/Planets.py
+++ Cards/Planets.py
@@ -29,5 +29,17 @@ class PlanetCard:
 #   These cards will be used in the shop and gameplay systems to upgrade specific poker hands.
 
 PLANETS = {
-
+    "Mercury": PlanetCard("Mercury", "levels up High Card", 4, 10, 1),
+    "Venus": PlanetCard("Venus", "levels up Pair", 4, 12, 1),
+    "Earth": PlanetCard("Earth", "levels up Two Pair", 5, 14, 2),
+    "Mars": PlanetCard("Mars", "levels up Three of a Kind", 6, 16, 2),
+    "Jupiter": PlanetCard("Jupiter", "levels up Straight", 6, 18, 3),
+    "Saturn": PlanetCard("Saturn", "levels up Flush", 7, 20, 3),
+    "Uranus": PlanetCard("Uranus", "levels up Full House", 7, 22, 4),
+    "Neptune": PlanetCard("Neptune", "levels up Four of a Kind", 8, 25, 4),
+    "Pluto": PlanetCard("Pluto", "levels up Straight Flush", 8, 28, 5),
+    "Planet X": PlanetCard("Planet X", "levels up Five of a Kind", 9, 30, 6),
+    "Black Hole": PlanetCard("Black Hole", "levels up all hands", 10, 35, 7),
+    "Sun": PlanetCard("Sun", "massively levels up all hands", 12, 40, 8)
 }
+
